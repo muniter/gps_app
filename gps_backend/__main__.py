36c4b532966de751c8f92cf4055eed65409d5513
Javier López <graulopezjavier@gmail.com>
2022-06-23 11:04:03 -0500

    refactor: prepare for "production" lol

--- gps_backend/__main__.py
+++ gps_backend/__main__.py
@@ -1,4 +1,5 @@
 from . import *
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.environ.get('ENV') != 'production')
--- gps_backend/utils.py
+++ gps_backend/utils.py
@@ -3,12 +3,12 @@ def dotenv_load():
     try:
         with open('.env') as f:
             for line in f:
-                key, value = line.strip().split('=', 1)
-                if value.startswith('"') and value.endswith('"'):
-                    value = value[1:-1]
-                elif value.startswith("'") and value.endswith("'"):
-                    value = value[1:-1]
-                res[key] = value
+                # Handle lines that do not have an equals sign
+                try:
+                    key, value = line.strip().split('=', 1)
+                    res[key] = value
+                except ValueError:
+                    pass
     except FileNotFoundError:
         pass
 
